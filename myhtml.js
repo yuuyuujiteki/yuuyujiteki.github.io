9887ff72be021455e61882d8f48a2922caec8f29
yuuyuujiteki <67782188+yuuyuujiteki@users.noreply.github.com>
2020-07-03 18:07:12 +0900

    Create myhtml.js

---
+++ myhtml.js
@@ -0,0 +1,15 @@
+var b = document.getElementById("iii");
+b.addEventListener(`click`,()=>{
+
+alert("歓喜");
+
+})
+
+const div = document.getElementsByClassName("ooo");
+div[0].animate({
+    color: ['#409EFF', '#FF409E']
+  }, {
+    duration: 1500,
+    iterations: Infinity
+  })
+  
